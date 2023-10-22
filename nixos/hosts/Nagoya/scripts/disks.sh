ba60e8cbcd6b8349378973f40b46c47efcdbf886
Nicholas Mason <nmt.mason@googlemail.com>
2023-10-22 15:49:48 +0100

    feat: add helper script for creating nixos disks

---
+++ nixos/hosts/Nagoya/scripts/disks.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+parted /dev/nvme0n1 -- mklabel gpt
+parted /dev/nvme0n1 -- mkpart primary 512MB -8GB
+parted /dev/nvme0n1 -- mkpart primary linux-swap -8GB 100%
+parted /dev/nvme0n1 -- mkpart ESP fat32 1MB 512MB
+parted /dev/nvme0n1 -- set 3 esp
+
+mkfs.ext4 -L nixos /dev/nvme0n1p1
+mkswap -L swap /dev/nvme0n1p2
+mkfs.fat -F 32 -n boot /dev/nvme0n1p3
+mount /dev/disk/by-label/nixos /mnt
+mkdir -p /mnt/{boot,media}
+mount /dev/disk/by-label/boot /mnt/boot
+mount /dev/disk/by-label/media /mnt/media
+swapon /dev/nvme0n1p2
